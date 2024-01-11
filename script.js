c369cee68cddcbe9719e85ddb52d27337f5a758b
KsuKuper <nekrashevich.k@mail.ru>
2024-01-11 20:43:27 +0300

    added a new file is script.js where showed the code of dynamic data typing in JavaScript as need in hw 2.1.2 and updated teoretical aspects in operator.md file

---
+++ script.js
@@ -0,0 +1,6 @@
+let x = 10; // число
+console.log("type of x is " + typeof x); // выведет "number"
+console.log("x is " + x); // = 10
+x = "Hello"; // строка
+console.log("type of x is " + typeof x); // выведет "string"
+console.log("x is " + x); // Hello
