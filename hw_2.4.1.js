5d71d25288e0225b3177e8e81089d9804ddb3c15
KsuKuper <nekrashevich.k@mail.ru>
2024-01-23 20:22:44 +0300

    added a new implementation option with storing calculations in an array, 2.4.1

--- hw_2.4.1.js
+++ hw_2.4.1.js
@@ -99,3 +99,47 @@ testCalculateExpenses();
 // const expenses = [1500, 500, 2000, 800, 1200, 3000, 1000, 5000, 900];
 // const total = calculateExpenses(expenses);
 // console.log(total); // Выведет: 12700
+
+// ВАРИАНТ 3
+
+// function calculateExpenses(yearlyExpences) {
+//   let totalExpenses = 0;
+//   yearlyExpences.forEach((expense) => {
+//     if (expense > 1000) {
+//       totalExpenses += expense;
+//     }
+//   });
+//   return totalExpenses;
+// }
+// function testCalculateExpenses() {
+//   let expencesExamples = [
+//     {
+//       yearlyExpences: [
+//         2500, 6000, 7000, 6000, 3, 9, 700, 8000, 655, 5700, 340, 5390,
+//       ],
+//     },
+//     {
+//       yearlyExpences: [
+//         500, 1000, 90000, 80, 980, 9000, 10, 400, 3000, 250, 45000, 1200,
+//       ],
+//     },
+//     {
+//       yearlyExpences: [
+//         20, 200, 8900, 3300, 2300, 1009, 1200, 2000, 670, 900, 900, 7000,
+//       ],
+//     },
+//   ];
+//   const results = []; // Массив для хранения результатов расчетов
+//   expencesExamples.forEach((example) => {
+//     const { yearlyExpences } = example;
+//     const totalExpenses = calculateExpenses(yearlyExpences);
+//     results.push(totalExpenses); // Добавление результата в массив results
+//   });
+//   console.log("Results:", results); // Вывод всех результатов
+// }
+// testCalculateExpenses();
+
+// В этой реализации вместо вывода каждого результата с помощью `console.log`, мы создаем массив `results`,
+// в который добавляем каждый результат расчета с помощью `results.push(totalExpenses)`.
+// Затем мы выводим все результаты, используя `console.log('Results:', results)`.
+// Теперь результаты будут храниться в массиве `results`.
