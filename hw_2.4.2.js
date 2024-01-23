e46b3442f23cc4a8ddc70613040a382186799252
KsuKuper <nekrashevich.k@mail.ru>
2024-01-23 19:20:34 +0300

    added output of months in which costs are less than or equal to 1000, as required in hw_ 2.4.2

---
+++ hw_2.4.2.js
@@ -0,0 +1,56 @@
+function calculateExpenses(yearlyExpenses) {
+  let totalExpenses = 0;
+  yearlyExpenses.forEach((expense) => {
+    if (expense > 1000) {
+      totalExpenses += expense;
+    }
+  });
+  return totalExpenses;
+}
+function testExpenseCalculation(expensesExamples) {
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  expensesExamples.forEach((example) => {
+    const yearlyExpenses = example.yearlyExpenses;
+    yearlyExpenses.forEach((expense, index) => {
+      if (expense <= 1000) {
+        const month = new Date(null, index).toLocaleDateString("en", {
+          month: "long",
+        });
+        console.log(`${months[index]} (${month}) expense is <= 1000`);
+      }
+    });
+    const totalExpenses = calculateExpenses(yearlyExpenses);
+    console.log(`Total expenses: $${totalExpenses}`);
+  });
+}
+const expensesExamples = [
+  {
+    yearlyExpenses: [
+      2500, 6000, 7000, 6000, 3, 9, 700, 8000, 655, 5700, 340, 5390,
+    ],
+  },
+  {
+    yearlyExpenses: [
+      500, 1000, 90000, 80, 980, 9000, 10, 400, 3000, 250, 45000, 1200,
+    ],
+  },
+  {
+    yearlyExpenses: [
+      20, 200, 8900, 3300, 2300, 1009, 1200, 2000, 670, 900, 900, 7000,
+    ],
+  },
+];
+testExpenseCalculation(expensesExamples);
